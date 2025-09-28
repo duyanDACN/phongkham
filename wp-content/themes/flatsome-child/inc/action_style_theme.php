326208aef5e56a713227f45f5b7a33f8d26cbfa9
An <duyantran@diemnhan.net>
2025-09-28 13:52:13 +0700

    update

--- wp-content/themes/flatsome-child/inc/action_style_theme.php
+++ wp-content/themes/flatsome-child/inc/action_style_theme.php
@@ -5,8 +5,27 @@ function addStylesTheme() {
     $theme_uri = get_stylesheet_directory_uri() . '/assets/css/';
     $version   = wp_get_theme()->get('Version');
 
+    // CSS chung
     wp_enqueue_style('style-global', $theme_uri . 'global.css', array('flatsome-main'), $version);
 
+    // Thêm thư viện Swiper CSS
+    wp_enqueue_style(
+        'swiper-css',
+        'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+        array(),
+        '11.0.0'
+    );
+
+    // Thêm thư viện Swiper JS
+    wp_enqueue_script(
+        'swiper-js',
+        'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+        array('jquery'),
+        '11.0.0',
+        true
+    );
+
+    // Chỉ load CSS riêng cho các trang survey
     if (is_page_template(array(
         'page-gad7.php',
         'page-phq9.php',
@@ -16,6 +35,7 @@ function addStylesTheme() {
         wp_enqueue_style('style-survey', $theme_uri . 'survey.css', array('flatsome-main'), $version);
     }
 
+    // Chỉ load CSS cho trang Bảng Giá
     if (is_page('bang-gia')) {
         wp_enqueue_style('style-price', $theme_uri . 'price.css', array('flatsome-main'), $version);
     }
